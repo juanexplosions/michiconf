79a2b126eeeb2c05ea90dfbc35eb8005b322eb24
juanexplosions <juanfeperaltaz@gmail.com>
2024-06-10 20:55:02 -0500

    feat highlights section and fix some style bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "/src/components/Hero/Hero.jsx";
 import Speakers from "/src/components/Speakers/Speakers.jsx";
+import Highlights from "/src/components/Highlights/Highlights.jsx";
 
 function App() {
   return (
---
+++ src/components/Highlights/Highlights.jsx
@@ -0,0 +1,47 @@
+import "./Highlights.scss";
+
+export default function Highlights() {
+  return (
+    <>
+      <section className="highlights">
+        <img src="/highlights-decoration.png" alt="" className="highlights__decoration"/>
+        <div className="highlights__selector">
+          <h2 className="highlights__title">
+            ¿Por qué no te puedes perder la MichiCONF?
+          </h2>
+          <p className="highlights__description">
+            Es más que solo una conf, es una fiesta gatuna desbordante de locura
+            y diversión.
+          </p>
+          <ul className="highlights__list">
+            <li className="highlights__item--active">
+              <p className="highlights__number--active">01</p>
+              <p className="highlights__text--active">Evento fuera de lo comun</p>
+            </li>
+            <li className="highlights__item">
+              <p className="highlights__number">01</p>
+              <p className="highlights__text">Evento fuera de lo comun</p>
+            </li>
+            <li className="highlights__item">
+              <p className="highlights__number">01</p>
+              <p className="highlights__text">Evento fuera de lo comun</p>
+            </li>
+            <li className="highlights__item">
+              <p className="highlights__number">01</p>
+              <p className="highlights__text">Evento fuera de lo comun</p>
+            </li>
+          </ul>
+        </div>
+        <div className="highlights__panel">
+          <div className="highlights__border"></div>
+          <p className="highlights__description-panel">
+            Cada charla, cada taller, y cada actividad están diseñados para
+            hacerte reír a carcajadas y dejarte boquiabierto.
+          </p>
+          <img src="/michi-01.png" alt="" className="highlights__cat" />
+          <img src="/michi-background-01.png" alt="" className="highlights__background-cat"/>
+        </div>
+      </section>
+    </>
+  );
+}
