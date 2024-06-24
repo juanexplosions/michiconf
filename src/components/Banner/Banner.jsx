9a54247f5cb10faf4b975287f194049e200b31ce
juanexplosions <juanfeperaltaz@gmail.com>
2024-06-23 22:00:16 -0500

    banner styles

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,8 +4,10 @@ import SignupButton from "../SignupButton/SignupButton";
 export default function Banner() {
   return (
     <section className="banner">
+      <div className="banner__gradient"></div>
+      <img src="/banner-light.png" alt="" className="banner__light"/>
       <h2 className="banner__title">Are you ready to get started? We got you covered!</h2>
-      <SignupButton />
+      <SignupButton className="banner_signup"/>
     </section>
   );
 }
